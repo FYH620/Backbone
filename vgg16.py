01c3963693d432515bc702652136b595f5c00553
FYH <2634028955@qq.com>
2021-11-25 23:26:13 +0800

    Create vgg16.py

---
+++ vgg16.py
@@ -0,0 +1,36 @@
+from torch import nn
+
+class VGG16(nn.Module):
+    def __init__(self,num_classes):
+        super(VGG16,self).__init__()
+        self.features=self._make_conv()
+        self.classifier=nn.Sequential(
+            nn.Linear(512*7*7,4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096,4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096,num_classes)
+        )
+    
+    def _make_conv(self):
+        layers=[]
+        in_dims=3
+        out_dims=64
+        for i in range(1,19):
+            if i in [3,6,10,14,18]:
+                layers.append(nn.MaxPool2d(kernel_size=2,stride=2))
+                if i!=14 and i!=18:
+                    out_dims=out_dims*2
+            else:
+                layers.append(nn.Conv2d(in_dims,out_dims,kernel_size=3,stride=1,padding=1))
+                layers.append(nn.ReLU(inplace=True))
+                in_dims=out_dims
+        return nn.Sequential(*layers)
+
+    def forward(self,x):
+        x=self.features(x)
+        x=x.view(x.size(0),-1)
+        x=self.classifier(x)
+        return x
