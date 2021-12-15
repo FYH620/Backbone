51da8ff0bc2b04e729de5c9c03b17024b8512e0a
FYH <2634028955@qq.com>
2021-12-15 22:17:50 +0800

    update

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-    self.shortcut = nn.Sequential(
-            nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=1, padding=0, bias=False),
-            nn.BatchNorm2d(out_planes),
-            )
